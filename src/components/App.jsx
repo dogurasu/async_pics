48f33b6bff16880d731de91d2b0b6dde49fef6e4
dogurasu <dugs134@gmail.com>
2020-10-13 21:09:28 -0700

    refactored App to a Class Based component to pass a callback \'onSearchSubmit\' for a way for SearchBar child to communicate with App parent

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 
-const App = () => {
-    return (
-        <div className="ui container" style={{marginTop: "1rem"}}>
-            <h1>App</h1>
-            <SearchBar />
-        </div>
-    );
+class App extends React.Component {
+
+    onSearchSubmit(term) {
+        console.log(term);
+    }
+
+    render() {
+        return (
+            <div className="ui container" style={{marginTop: "1rem"}}>
+                <h1>App</h1>
+                <SearchBar onSubmit={this.onSearchSubmit}/>
+            </div>
+        );
+    }
 }
 
 export default App;
